f2ff09ccae5ccd26f2fb1e665800a549acf2e63f
Sven Strittmatter <ich@weltraumschaf.de>
2018-01-29 21:28:53 +0100

    Read a file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2 {
+        error("Exactly one argument expected!");
+        return;
+    }
+
+    let file = &args[1];
+    println!("Running file {} ...", file);
+
+    let mut f = match File::open(file) {
+        Ok(f) => f,
+        Err(_) => {
+            error("Failed to read file!");
+            return;
+        }
+    };
+
+    let mut contents = String::new();
+    match f.read_to_string(&mut contents) {
+        Ok(contents) => contents,
+        Err(_) => {
+            error("Something went wrong reading the file!");
+            return;
+        }
+    };
+
+    println!("With text:\n{}", contents);
+}
+
+fn error(msg: &str) {
+    println!("ERROR: {}", msg);
 }
