01d5075ba5e8330dd0cf6081ce92befe14f1a356
Sven Strittmatter <ich@weltraumschaf.de>
2018-02-01 12:09:11 +0100

    Implement character stream

--- src/lexer/character_stream.rs
+++ src/lexer/character_stream.rs
@@ -1,7 +1,9 @@
+use lexer::Position;
+
 #[derive(Debug)]
 struct CharacterStream {
     input: String,
-    index: u64,
+    index: usize,
     line: u64,
     column: u64,
     new_line_seen: bool
@@ -18,16 +20,53 @@ impl CharacterStream {
         }
     }
 
-    fn next(&self) -> char {
-        panic!("Not implemented!");
+    fn next(&mut self) -> Option<char> {
+        if !self.has_next() {
+            return None
+        }
+
+        if self.position().at(Position::null()) {
+            self.line = 1;
+            self.column = 1;
+        }
+
+        let current = self.current();
+        self.index += 1;
+
+        if self.new_line_seen {
+            self.line += 1;
+            self.column = 1;
+            self.new_line_seen = false;
+        } else {
+            self.column += 1;
+        }
+
+        if let Some(c) = current {
+            if '\n' == c {
+                self.new_line_seen = true;
+            }
+        }
+
+        current
     }
 
     fn has_next(&self) -> bool {
-        false
+        self.index < self.input.len()
     }
 
-    fn peek(&self) -> char {
-        panic!("Not implemented!");
+    fn current(&mut self) -> Option<char> {
+        self.input.chars().nth(self.index)
+    }
+
+    fn peek(&mut self) -> Option<char> {
+        let index_backup = self.index;
+        let c = self.next();
+        self.index = index_backup;
+        c
+    }
+
+    fn position(&self) -> Position {
+        Position { line: self.line, column: self.column }
     }
 }
 
@@ -36,20 +75,81 @@ mod tests {
     use super::*;
     use hamcrest::prelude::*;
 
-    #[test]
-    #[ignore]
-    fn next() {
-        assert_eq!(2 + 2, 4);
+    fn crete_sut(input: &str) -> CharacterStream {
+        CharacterStream::new(String::from(input))
     }
 
     #[test]
-    fn has_next() {
-        let sut = CharacterStream::new(String::from("Hello,\nWorld!"));
-        assert_that!(1, is(equal_to(1)));
-//        assert_that(sut.has_next(), is(false));
+    fn has_no_next_for_empty_string() {
+        let sut = crete_sut("");
+
+        assert_that!(sut.has_next(), is(false));
     }
 
     #[test]
-    #[ignore]
-    fn peek() {}
+    fn next_for_empty_string() {
+        let mut sut = crete_sut("");
+
+        assert_that!(sut.next(), is(equal_to(None)));
+    }
+
+    #[test]
+    fn current_for_empty_string() {
+        let mut sut = crete_sut("");
+
+        assert_that!(sut.current(), is(equal_to(None)));
+    }
+
+    #[test]
+    fn iterate_through_string() {
+        let mut sut = crete_sut("Hello,\nWorld!");
+
+        assert_that!(sut.has_next(), is(equal_to(true)));
+        assert_that!(sut.next(), is(equal_to(Some('H'))));
+        assert_that!(sut.has_next(), is(equal_to(true)));
+        assert_that!(sut.next(), is(equal_to(Some('e'))));
+        assert_that!(sut.has_next(), is(equal_to(true)));
+        assert_that!(sut.next(), is(equal_to(Some('l'))));
+        assert_that!(sut.has_next(), is(equal_to(true)));
+        assert_that!(sut.next(), is(equal_to(Some('l'))));
+        assert_that!(sut.has_next(), is(equal_to(true)));
+        assert_that!(sut.next(), is(equal_to(Some('o'))));
+        assert_that!(sut.has_next(), is(equal_to(true)));
+        assert_that!(sut.next(), is(equal_to(Some(','))));
+        assert_that!(sut.has_next(), is(equal_to(true)));
+        assert_that!(sut.next(), is(equal_to(Some('\n'))));
+        assert_that!(sut.has_next(), is(equal_to(true)));
+        assert_that!(sut.next(), is(equal_to(Some('W'))));
+        assert_that!(sut.has_next(), is(equal_to(true)));
+        assert_that!(sut.next(), is(equal_to(Some('o'))));
+        assert_that!(sut.has_next(), is(equal_to(true)));
+        assert_that!(sut.next(), is(equal_to(Some('r'))));
+        assert_that!(sut.has_next(), is(equal_to(true)));
+        assert_that!(sut.next(), is(equal_to(Some('l'))));
+        assert_that!(sut.has_next(), is(equal_to(true)));
+        assert_that!(sut.next(), is(equal_to(Some('d'))));
+        assert_that!(sut.has_next(), is(equal_to(true)));
+        assert_that!(sut.next(), is(equal_to(Some('!'))));
+        assert_that!(sut.has_next(), is(equal_to(false)));
+        assert_that!(sut.next(), is(equal_to(None)));
+    }
+
+    #[test]
+    fn peek() {
+        let mut sut = crete_sut("Hello");
+
+        sut.next();
+
+        assert_that!(sut.peek(), is(equal_to(Some('e'))));
+        assert_that!(sut.next(), is(equal_to(Some('e'))));
+
+        assert_that!(sut.peek(), is(equal_to(Some('l'))));
+        assert_that!(sut.next(), is(equal_to(Some('l'))));
+
+        sut.next();
+
+        assert_that!(sut.peek(), is(equal_to(Some('o'))));
+        assert_that!(sut.next(), is(equal_to(Some('o'))));
+        assert_that!(sut.peek(), is(equal_to(None)));
+    }
 }
--- src/lexer/mod.rs
+++ src/lexer/mod.rs
@@ -1,13 +1,47 @@
-
 mod character_stream;
 mod lexer;
 
 #[derive(Debug)]
-enum Token {
-
-}
+enum Token {}
 
 #[derive(Debug)]
 struct Position {
+    line: u64,
+    column: u64,
+}
 
+impl Position {
+    fn null() -> Position {
+        Position::new(0, 0)
+    }
+
+    fn new(line: u64, column: u64) -> Position {
+        Position { line, column }
+    }
+
+    fn at(&self, p: Position) -> bool {
+        p.line == self.line && p.column == self.column
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use hamcrest::prelude::*;
+
+    #[test]
+    fn at() {
+        let sut = Position::new(42, 23);
+
+        assert_that!(sut.at(Position::null()), is(false));
+        assert_that!(sut.at(Position::new(42, 23)), is(true));
+    }
+
+    #[test]
+    fn null() {
+        let sut = Position::null();
+
+        assert_that(sut.at(Position::null()), is(true));
+        assert_that(sut.at(Position::new(42, 23)), is(false));
+    }
 }
