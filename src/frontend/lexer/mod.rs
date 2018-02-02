c286a1b2205e0db09b999bd008c79992b9123e60
Sven Strittmatter <ich@weltraumschaf.de>
2018-02-03 00:10:38 +0100

    Annotate unimplemented lexer parts

--- src/frontend/lexer/mod.rs
+++ src/frontend/lexer/mod.rs
@@ -37,15 +37,20 @@ impl Lexer {
             let current = self.input.current();
 
             if CharacterHelper::is_alphabetic(current) {
+                // scan for boolean/keyword/identifier
                 token = IdentifierLexer::new().scan(&mut self.input);
             } else if CharacterHelper::is_numeric(current) {
+                // scan for integer and real number
                 token = NumberLexer::new().scan(&mut self.input);
             } else if CharacterHelper::is_double_quote(current) {
-// TODO
+                // TODO scan for string literal
+                panic!("not implemented yet: string literal");
             } else if CharacterHelper::is_single_quote(current) {
-// TODO
+                // TODO scan for single character literal
+                panic!("not implemented yet: character literal");
             } else if CharacterHelper::is_operator(current) {
-// TODO
+                // TODO scan for operator or delimiter
+                panic!("not implemented yet: operator");
             } else if CharacterHelper::is_white_space(current) {
                 // ignore white spaces
             } else if CharacterHelper::is_new_line(current) {
