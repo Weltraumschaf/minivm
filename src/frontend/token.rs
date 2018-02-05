1590e1e5c6bd827d431c40967b6dd83fe8484df2
Sven Strittmatter <ich@weltraumschaf.de>
2018-02-05 20:06:19 +0100

    Remove unnecessary parens in token display

--- src/frontend/token.rs
+++ src/frontend/token.rs
@@ -22,7 +22,7 @@ impl Token {
 
 impl fmt::Display for Token {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "({} '{}' {})", self.token_type, self.literal, self.position)
+        write!(f, "{} '{}' {}", self.token_type, self.literal, self.position)
     }
 }
 
