ceacd46c5c391c2083011ec3dc309633ba9e4134
Sven Strittmatter <ich@weltraumschaf.de>
2018-02-26 20:03:09 +0100

    Move example visitor

--- src/intermediate/mod.rs
+++ src/intermediate/mod.rs
@@ -3,30 +3,3 @@
 mod ast;
 mod visitor;
 
-use intermediate::visitor::*;
-use intermediate::ast::*;
-
-// An example concrete implementation - walks the AST interpreting it as code.
-struct Interpreter;
-
-impl Visitor<i64> for Interpreter {
-    fn visit_identifier(&mut self, n: &Identifier) -> i64 { panic!() }
-
-    fn visit_statement(&mut self, s: &Statement) -> i64 {
-        match *s {
-            Statement::Expression(ref e) => self.visit_expression(e),
-            Statement::Assignment(..) => unimplemented!(),
-            _ => unimplemented!(),
-        }
-    }
-
-    fn visit_expression(&mut self, e: &Expression) -> i64 {
-        match *e {
-            Expression::Integer(n) => n,
-            Expression::BinaryOperation(ref op, ref lhs, ref rhs) => {
-                self.visit_expression(lhs) + self.visit_expression(rhs)
-            },
-            _ => unimplemented!(),
-        }
-    }
-}
--- src/intermediate/visitor.rs
+++ src/intermediate/visitor.rs
@@ -6,3 +6,28 @@ pub trait Visitor<T> {
     fn visit_statement(&mut self, s: &Statement) -> T;
     fn visit_expression(&mut self, e: &Expression) -> T;
 }
+
+// An example concrete implementation - walks the AST interpreting it as code.
+struct Interpreter;
+
+impl Visitor<i64> for Interpreter {
+    fn visit_identifier(&mut self, n: &Identifier) -> i64 { panic!() }
+
+    fn visit_statement(&mut self, s: &Statement) -> i64 {
+        match *s {
+            Statement::Expression(ref e) => self.visit_expression(e),
+            Statement::Assignment(..) => unimplemented!(),
+            _ => unimplemented!(),
+        }
+    }
+
+    fn visit_expression(&mut self, e: &Expression) -> i64 {
+        match *e {
+            Expression::Integer(n) => n,
+            Expression::BinaryOperation(ref op, ref lhs, ref rhs) => {
+                self.visit_expression(lhs) + self.visit_expression(rhs)
+            },
+            _ => unimplemented!(),
+        }
+    }
+}
